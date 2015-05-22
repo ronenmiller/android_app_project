c7a30181686d41b22b8666e7529fd1508d3a17bf
ronenmiller <ronen.miller1@gmail.com>
2015-05-22 18:42:31 +0300

    initial commit

---
+++ sql/add_tour.sql
@@ -0,0 +1,38 @@
+CREATE OR REPLACE FUNCTION add_tour(p_u_id uuid, p_city_name VARCHAR(255), p_region_name VARCHAR (255), p_country_name VARCHAR(255), p_t_duration NUMERIC, p_t_description VARCHAR(255), p_t_photos bytea[], p_t_languages INTEGER)
+	RETURNS VOID AS 
+$$
+DECLARE
+	v_city_id		VARCHAR(255);
+	v_tour_num		INTEGER;
+	v_exception_err TEXT;
+BEGIN
+	v_city_id = find_cityid_by_name(p_city_name, p_region_name, p_country_name);
+	
+	SELECT max(tours.t_id) INTO v_tour_num FROM tours;
+	IF v_tour_num IS NULL THEN
+		v_tour_num = 1;
+	ELSE
+		v_tour_num = v_tour_num + 1;
+	END IF;
+	
+	BEGIN
+		INSERT INTO tours(u_id, t_id, t_cityid, t_duration, t_description, t_photos, t_languages, t_available)
+		VALUES (p_u_id, v_tour_num, v_city_id, p_t_duration, p_t_description, p_t_photos, p_t_languages, B'0');
+
+	EXCEPTION 
+		WHEN foreign_key_violation THEN
+			GET STACKED DIAGNOSTICS v_exception_err = MESSAGE_TEXT;
+			RAISE foreign_key_violation USING MESSAGE = v_exception_err;
+		WHEN unique_violation THEN
+			GET STACKED DIAGNOSTICS v_exception_err = MESSAGE_TEXT;
+			RAISE unique_violation USING MESSAGE = v_exception_err;
+		WHEN OTHERS THEN
+			GET STACKED DIAGNOSTICS v_exception_err = MESSAGE_TEXT;
+			RAISE EXCEPTION USING MESSAGE = v_exception_err;
+
+	END;
+
+	RETURN;
+END;
+$$ 
+LANGUAGE 'plpgsql';
---
+++ sql/search_tour_by_guide.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE FUNCTION search_tour_by_guide(v_guide_name VARCHAR(255))
+	RETURNS SETOF view_tours AS 
+$$
+DECLARE
+	v_guide_id	      uuid;
+	v_guide_db_name   VARCHAR(80);
+BEGIN
+	SELECT users.u_id, users.u_name
+	INTO v_guide_id, v_guide_db_name
+	FROM users
+	WHERE users.u_name = v_guide_name;
+
+	IF NOT FOUND THEN
+        RAISE EXCEPTION 'No guide named % found', $1;
+    END IF;
+	
+	RETURN QUERY 
+	SELECT v_guide_db_name, tours.t_id, cities.city, states.region, country.country, tours.t_rating, languages.lang_name, tours.t_duration, tours.t_description, tours.t_photos, tours.t_comments, tours.t_available
+	FROM users, tours, languages, cities, states, country
+	WHERE tours.u_id = v_guide_id
+	AND cities.cityid = tours.t_cityid
+	AND states.regionid = cities.regionid
+	AND country.countryid = cities.countryid
+	AND languages.lang_id = tours.t_languages;
+	
+	RETURN;
+END;
+$$ 
+LANGUAGE 'plpgsql';
