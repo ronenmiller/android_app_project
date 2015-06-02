113f0bebb8f133cb1ef41ec79f88d1733f6f4360
ronenmiller <ronen.miller1@gmail.com>
2015-06-02 23:28:11 +0300

    for Gson/JSON support

--- eclipse_ee_ws/ToursAppServer/src/tours_app_client/ClientExample.java
+++ eclipse_ee_ws/ToursAppServer/src/tours_app_client/ClientExample.java
@@ -7,47 +7,42 @@ import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-//import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
+import com.google.gson.Gson;
+
+
+
 public class ClientExample {
 
     public static void main(String[] args) throws Exception {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         try {
-        	/*
-            HttpGet httpGet = new HttpGet("http://targethost/homepage");
-            CloseableHttpResponse response1 = httpclient.execute(httpGet);
-            // The underlying HTTP connection is still held by the response object
-            // to allow the response content to be streamed directly from the network socket.
-            // In order to ensure correct deallocation of system resources
-            // the user MUST call CloseableHttpResponse#close() from a finally clause.
-            // Please note that if response content is not fully consumed the underlying
-            // connection cannot be safely re-used and will be shut down and discarded
-            // by the connection manager.
-            try {
-                System.out.println(response1.getStatusLine());
-                HttpEntity entity1 = response1.getEntity();
-                // do something useful with the response body
-                // and ensure it is fully consumed
-                EntityUtils.consume(entity1);
-            } finally {
-                response1.close();
-            }
-            */
-
+        	// Instanciate Gson 
+        	Gson gson = new Gson();
+        	// JSON string
+        	String json;
+        	
+        	// http post 
             HttpPost httpPost = new HttpPost("http://localhost:8080/ToursAppServer/tours_slet");
     		
+            // create Name<->Value pairs list 
             List <NameValuePair> nvps = new ArrayList <NameValuePair>();
-            nvps.add(new BasicNameValuePair("reqType", "rm"));
+            nvps.add(new BasicNameValuePair("reqType", "add"));
             nvps.add(new BasicNameValuePair("uname", "HollyShit"));
             nvps.add(new BasicNameValuePair("email", "holyshit@gmail.com"));
             nvps.add(new BasicNameValuePair("phnum", "0545325689"));
             nvps.add(new BasicNameValuePair("password", "secret2013"));
+            
+            // convert list to JSON format string
+            json = gson.toJson(nvps);
+            
+            System.out.println(json);
+            /*
             httpPost.setEntity(new UrlEncodedFormEntity(nvps));
             CloseableHttpResponse response2 = httpclient.execute(httpPost);
 
@@ -57,11 +52,13 @@ public class ClientExample {
                 // do something useful with the response body
                 // and ensure it is fully consumed
                 EntityUtils.consume(entity2);
+                
             } finally {
-                response2.close();
+                //response2.close();
             }
+            */
         } finally {
-            httpclient.close();
+            // httpclient.close();
         }
     }
 
