4c773f2dfcdaa109024cacda63a852ff8d12526d
liron88 <anavi.liron@gmail.com>
2015-07-27 21:09:41 +0300

    Add database functions related to slots

--- eclipse_ee_ws/ToursAppServer/src/tours_app_server/Message.java
+++ eclipse_ee_ws/ToursAppServer/src/tours_app_server/Message.java
@@ -51,11 +51,17 @@ public class Message {
 		public static final String USER_PHONE_KEY = "UserPhone";
 		public static final String USER_TYPE_KEY = "UserType";
 		
+		public static final String TOURS_ID_KEY = "TourID";
 		public static final String TOURS_DURATION_KEY = "ToursDuration";
+		public static final String TOURS_LOCATION_KEY = "ToursLocation";
 		public static final String TOURS_DESCRIPTION_KEY = "ToursDescription";
 		public static final String TOURS_PHOTOS_KEY = "ToursPhotos";
 		public static final String TOURS_LANGUAGE_KEY = "ToursLanguage";
 		public static final String TOURS_BY_CITY_KEY = "ToursByCity";
+		
+		public static final String SLOT_DATE_KEY = "SlotDate";
+		public static final String SLOT_TIME_KEY = "SlotTime";
+		public static final String SLOT_NUM_VACANT_KEY = "SlotVacant";
 		public static final String SLOTS_BY_CITY_KEY = "SlotsByCity";
 	}
 
